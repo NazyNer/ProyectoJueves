438d29df321842fc71db5a9c7daa768dfb0e9b9d
Arcangel Maldonado <nemaldonado663@gmail.com>
2023-08-10 15:04:45 -0300

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoJueves.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
